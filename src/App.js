330daaa23e2f178205a6b7351dbd20b80e89677f
Rose Wanjohi <rosewanjohi24@gmail.com>
2022-01-27 20:08:53 +0300

    add connect wallet, change network and address functionality.

--- src/App.js
+++ src/App.js
@@ -9,40 +9,62 @@ import DaiToken from './artifacts/contracts/DaiToken.sol/DaiToken.json';
 import {gameAddress, tokenAddress} from './config'
 
 function App() {
-  const [address, setAddress] = useState('')
-  const [network, setNetwork] = useState('')
-  let gameContract;
-  let daiToken;
-  let amount;
-  let signerAddress;
+  const [address, setAddress] = useState('');
+  const [network, setNetwork] = useState('');
+  const [gameContract, setGameContract] = useState(undefined);
+  const [daiToken, setDaiToken] = useState(undefined);
+  const [amount, setAmount] = useState(undefined);
+  const [connected, setConnected] = useState(false)
+  const [complete, setComplete] = useState(true)
+
+  async function connectWallet() {
+    if(window.ethereum) {
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+      setAddress(accounts[0])
+      const web3Modal = new Web3Modal()
+      const connection = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(connection, 'any')
+      const {name} = await provider.getNetwork()
+      setNetwork(name)
+      const signer = await provider.getSigner()
+      const gameContract = new ethers.Contract(gameAddress, Game.abi, signer)
+      setGameContract(gameContract)
+      const daiToken = new ethers.Contract(tokenAddress, DaiToken.abi, signer)
+      setDaiToken(daiToken)
+      const amount = ethers.utils.parseUnits('1', 'ether')
+      setAmount(amount)
+      setConnected(true)
+    }
+  }
 
   const handleAccountChanged = (accounts) => {
     setAddress(accounts[0])
   }
 
+  const networkChanged = async (chainId) => {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection, 'any')
+    const {name} = await provider.getNetwork()
+    setNetwork(name)
+    console.log(name)
+  }
+
   useEffect(() => {
-    window.ethereum.on('accountsChanged', handleAccountChanged)
-    loadWeb3()
+    window.ethereum.on('accountsChanged', handleAccountChanged);
+    window.ethereum.on("chainChanged", networkChanged);
 
     return () => {
       window.ethereum.removeListener('accountsChanged', handleAccountChanged);
+      window.ethereum.removeListener("chainChanged", networkChanged);
     }
   },[])
 
-
-  async function loadWeb3() {
-    const web3Modal = new Web3Modal()
-    const connection = await web3Modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
-    signerAddress = await signer.getAddress()
-    setAddress(signerAddress)
-    gameContract = new ethers.Contract(gameAddress, Game.abi, signer)
-    daiToken = new ethers.Contract(tokenAddress, DaiToken.abi, signer)
-    amount = ethers.utils.parseUnits('1', 'ether')
-    // ***Check Gas Estimates for txs***
-    // const estimate = await gameContract.estimateGas.withdrawGame(amount)
-    // console.log(estimate.toString())
+  async function switchNetwork() {
+    await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{chainId: '0x2a'}]
+    })
   }
 
 
@@ -81,15 +103,37 @@ function App() {
     await gameContract.withdrawGame(amount, {gasLimit:300000, gasPrice:2500000000})
   }
 
-
   return (
     <div className="App">
-    <h2>Hello World!</h2>
-    <h4>Network:{network}</h4>
-    <h4>Address: {address}</h4>
-    <button onClick={approveGame}>Approve Game</button>
-    <button onClick={joinGame}>Join Game</button>
-    <button onClick={withdrawGame}>Withdraw Game</button>
+    <h2>Saving Game</h2>
+    {connected ? (
+      <>
+        <h4>Address: {address}</h4>
+        <h4>Network: {network}</h4>
+        {network === 'kovan' ? (
+          <>
+          <div>
+          <button onClick={approveGame}>Approve Game</button>
+          </div>
+          <div>
+          <button onClick={joinGame}>Join Game</button>
+          </div>
+          <div>
+          <button onClick={withdrawGame}>Withdraw Game</button>
+          </div>
+          </>
+        ) : (
+          <>
+          <h5>Please switch to Kovan network</h5>
+          <button onClick={switchNetwork}>Switch to Kovan</button>
+          </>
+        )}
+      </>
+    ) : (
+      <>
+      <button onClick={connectWallet}>Connect Wallet</button>
+      </>
+    )}
     </div>
   );
 }
