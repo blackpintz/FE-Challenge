d19dfd9fbc5577a2ef4737d0179affa73ecc09f7
Rose Wanjohi <rosewanjohi24@gmail.com>
2022-02-15 17:24:11 +0300

    update cypress tests.

--- tests/e2e/specs/app-spec.js
+++ tests/e2e/specs/app-spec.js
@@ -3,22 +3,26 @@ describe("GoodGhosting game process", () => {
         cy.visit('/')
         cy.get('#connect').click()
         cy.acceptMetamaskAccess()
+        cy.get('h2').contains('Please approve before joining the game!')
     })
 
     it('approves game', () => {
         cy.get('#approve').click()
         cy.confirmMetamaskPermissionToSpend()
+        cy.get('h2').contains('You are approved. Please join the game!')
         cy.wait(30000);
     })
 
     it('joins game', () => {
         cy.get('#join').click()
         cy.confirmMetamaskPermissionToSpend()
+        cy.get('h2').contains('You are a player!')
         cy.wait(30000)
     })
 
     it('withdraws from game', () => {
         cy.get('#withdraw').click()
         cy.confirmMetamaskPermissionToSpend()
+        cy.get('h2').contains('Please approve before joining the game!')
     })
 })
